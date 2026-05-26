b4efa2fae81862ae6c086b87c7d8bb5c77f89393
Ilay Mangel <ilaymangel@campus.technion.ac.il>
2026-05-26 11:34:27 +0300

    Ilay training to commit amd push

--- v3/run_app.py
+++ v3/run_app.py
@@ -18,7 +18,6 @@ if __package__ in (None, ""):
     sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from v3.gui.app import main
-#######
 
 # ===========================================================
 #  Easy startup toggle  (remember to SAVE the file!)
